e05f276a53b3275eae85c0cf6b64512edf4eeb50
Eduu115 <serranotrenado.e@gmail.com>
2025-04-05 22:30:03 +0200

    create database

---
+++ sql/setup.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS contacts_app;
+
+CREATE DATABASE contacts_app;
+
+USE contacts_app;
+
+CREATE table contacts(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    phone_number VARCHAR(255)
+);
+
+INSERT INTO contacts (name, phone_number) VALUES("Pepe", "123456789");
+INSERT INTO contacts (name, phone_number) VALUES("Edu", "637685657");
+INSERT INTO contacts (name, phone_number) VALUES("Jose", "987654321");
